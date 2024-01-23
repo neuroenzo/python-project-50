819abe3a020815e44a7324abb647e0fbb1e917cb
neuroenzo <neuroenzo@gmail.com>
2024-01-23 14:00:45 +0300

    add format in arguments for cli

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,6 +6,7 @@ def main():
         description='Compares two configuration files and shows a difference.')
     diff.add_argument('first_file')
     diff.add_argument('second_file')
+    diff.add_argument('-f', '--format', help='set format of output')
 
     diff.parse_args()
 
