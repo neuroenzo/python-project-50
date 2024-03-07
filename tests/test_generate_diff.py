8317a2e299e7cf7e43dfd07e77ecae420156ed6c
neuroenzo <neuroenzo@gmail.com>
2024-03-07 20:51:56 +0300

    yet another fix tests path

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -8,8 +8,8 @@ from gendiff.formatters.stylish import stylish_formatter
 
 @pytest.mark.parametrize(
     'flat1, flat2',
-    [('python-project-50/tests/fixtures/flat1.json',
-      'python-project-50/tests/fixtures/flat2.json')]
+    [('./tests/fixtures/flat1.json',
+      './tests/fixtures/flat2.json')]
 )
 def test_generate_flat_diff(flat1, flat2):
     diff = generate_diff(flat1, flat2)
@@ -18,12 +18,12 @@ def test_generate_flat_diff(flat1, flat2):
 
 @pytest.mark.parametrize(
     'nested1, nested2',
-    [('python-project-50/tests/fixtures/nested1.json',
-      'python-project-50/tests/fixtures/nested2.json'),
-     ('python-project-50/tests/fixtures/nested1.json',
-      'python-project-50/tests/fixtures/nested2.yml'),
-     ('python-project-50/tests/fixtures/nested1.yaml',
-      'python-project-50/tests/fixtures/nested2.yml')]
+    [('./tests/fixtures/nested1.json',
+      './tests/fixtures/fixtures/nested2.json'),
+     ('./tests/fixtures/nested1.json',
+      './tests/fixtures/nested2.yml'),
+     ('./tests/fixtures/nested1.yaml',
+      './tests/fixtures/nested2.yml')]
 )
 def test_generate_nested_diff(nested1, nested2):
     diff = generate_diff(nested1, nested2)
