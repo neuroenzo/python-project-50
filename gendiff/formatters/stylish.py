42a6ff1dc1cfb890f1c1cd845bae0baf1bfa213c
neuroenzo <neuroenzo@gmail.com>
2024-03-11 14:14:31 +0300

    add indents in function

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -8,22 +8,34 @@ def stylish_formatter(source, depth=1):
     for item in source:
         match item['status']:
             case 'nested':
-                result.append(f"{INDENTS*2*depth}{item['key']}:"
-                              f" {stylish_formatter(item['value'], depth+1)}")
+                result.append(
+                    f"{INDENTS*2*depth}{item['key']}:"
+                    f" {stylish_formatter(item['value'], depth+1)}"
+                )
             case 'added':
-                result.append(f"{INDENTS*(2*depth-1)}{PLUS}{item['key']}:"
-                              f" {display_like_json(item['value'], depth + 1)}")
+                result.append(
+                    f"{INDENTS*(2*depth-1)}{PLUS}{item['key']}:"
+                    f" {display_like_json(item['value'], depth + 1)}"
+                )
             case 'deleted':
-                result.append(f"{INDENTS*(2*depth-1)}{MINUS}{item['key']}:"
-                              f" {display_like_json(item['value'], depth + 1)}")
+                result.append(
+                    f"{INDENTS*(2*depth-1)}{MINUS}{item['key']}:"
+                    f" {display_like_json(item['value'], depth + 1)}"
+                )
             case 'unchanged':
-                result.append(f"{INDENTS*2*depth}{item['key']}:"
-                              f" {display_like_json(item['value'], depth + 1)}")
+                result.append(
+                    f"{INDENTS*2*depth}{item['key']}:"
+                    f" {display_like_json(item['value'], depth + 1)}"
+                )
             case 'changed':
-                result.append(f"{INDENTS*(2*depth-1)}{MINUS}{item['key']}:"
-                              f" {display_like_json(item['old value'], depth + 1)}")
-                result.append(f"{INDENTS*(2*depth-1)}{PLUS}{item['key']}:"
-                              f" {display_like_json(item['new value'], depth + 1)}")
+                result.append(
+                    f"{INDENTS*(2*depth-1)}{MINUS}{item['key']}:"
+                    f" {display_like_json(item['old value'], depth + 1)}"
+                )
+                result.append(
+                    f"{INDENTS*(2*depth-1)}{PLUS}{item['key']}:"
+                    f" {display_like_json(item['new value'], depth + 1)}"
+                )
     result.append(f'{INDENTS*2*(depth-1)}}}')
 
     return '\n'.join(result)
@@ -39,8 +51,10 @@ def display_like_json(structure, depth):
         case dict():
             result = ['{']
             for key, value in structure.items():
-                result.append(f'{INDENTS*2*depth}{key}:'
-                              f' {display_like_json(value, depth + 1)}')
+                result.append(
+                    f'{INDENTS*2*depth}{key}:'
+                    f' {display_like_json(value, depth + 1)}'
+                )
             result.append(f'{INDENTS*2*(depth-1)}}}')
             return '\n'.join(result)
         case _:
