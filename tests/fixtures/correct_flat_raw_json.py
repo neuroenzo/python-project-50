846c7110b20b716473fdcf5ded7b552a10481fa7
neuroenzo <neuroenzo@gmail.com>
2024-03-17 01:15:32 +0300

    add tests for raw json format

---
+++ tests/fixtures/correct_flat_raw_json.py
@@ -0,0 +1,30 @@
+correct_flat_raw_json = \
+"""
+[
+    {
+        "status": "deleted",
+        "key": "follow",
+        "value": false
+    },
+    {
+        "status": "unchanged",
+        "key": "host",
+        "value": "hexlet.io"
+    },
+    {
+        "status": "deleted",
+        "key": "proxy",
+        "value": "123.234.53.22"
+    },
+    {
+        "status": "changed",
+        "key": "timeout",
+        "old value": 50,
+        "new value": 20
+    },
+    {
+        "status": "added",
+        "key": "verbose",
+        "value": true
+    }
+]"""
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -3,6 +3,8 @@ import pytest
 from fixtures.correct_flat_stylish import correct_flat_stylish_structure
 from fixtures.correct_nested_stylish import correct_nested_stylish_structure
 from fixtures.correct_nested_plain import correct_nested_plain_structure
+from fixtures.correct_nested_raw_json import correct_nested_raw_json
+from fixtures.correct_flat_raw_json import correct_flat_raw_json
 from gendiff import generate_diff
 
 
@@ -52,3 +54,33 @@ def test_generate_nested_plain_diff(nested1, nested2, plain_format):
     diff = generate_diff(nested1, nested2, plain_format)
 
     assert diff == correct_nested_plain_structure
+
+
+@pytest.mark.parametrize(
+    'flat1, flat2, raw_json_format',
+    [('./tests/fixtures/flat1.json',
+      './tests/fixtures/flat2.json',
+      'json')]
+)
+def test_generate_nested_json_diff(flat1, flat2, raw_json_format):
+    diff = generate_diff(flat1, flat2, raw_json_format)
+
+    assert diff == correct_flat_raw_json
+
+
+@pytest.mark.parametrize(
+    'nested1, nested2, raw_json_format',
+    [('./tests/fixtures/nested1.json',
+      './tests/fixtures/nested2.json',
+      'json'),
+     ('./tests/fixtures/nested1.json',
+      './tests/fixtures/nested2.yml',
+      'json'),
+     ('./tests/fixtures/nested1.yaml',
+      './tests/fixtures/nested2.yml',
+      'json')]
+)
+def test_generate_nested_json_diff(nested1, nested2, raw_json_format):
+    diff = generate_diff(nested1, nested2, raw_json_format)
+
+    assert diff == correct_nested_raw_json
