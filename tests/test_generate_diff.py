ab646b886ac145c6265fbbf86a19c1a9fa073b27
neuroenzo <neuroenzo@gmail.com>
2024-05-03 14:04:38 +0300

    optimize test: reduce useless complexity

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -18,100 +18,32 @@ def form_path_to_test_file(file_name):
 @pytest.mark.parametrize(
     'nested1,'
     'nested2,'
-    'stylish_format,'
-    'correct_nested_stylish',
+    'actual_format,'
+    'expected_format',
     [('nested1.json',
       'nested2.json',
       'stylish',
       'correct_nested_stylish'),
      ('nested1.json',
       'nested2.yml',
-      'stylish',
-      'correct_nested_stylish'),
-     ('nested1.yaml',
-      'nested2.yml',
-      'stylish',
-      'correct_nested_stylish')]
-)
-def test_generating_nested_stylish_diff(nested1,
-                                        nested2,
-                                        stylish_format,
-                                        correct_nested_stylish):
-    diff = generate_diff(
-        form_path_to_test_file(nested1),
-        form_path_to_test_file(nested2),
-        stylish_format
-    )
-
-    with open(form_path_to_test_file(correct_nested_stylish), 'r') as file:
-        correct_structure = file.read()
-
-        assert diff == correct_structure
-
-
-@pytest.mark.parametrize(
-    'nested1,'
-    'nested2,'
-    'plain_format,'
-    'correct_nested_plain',
-    [('nested1.json',
-      'nested2.json',
       'plain',
       'correct_nested_plain'),
-     ('nested1.json',
-      'nested2.yml',
-      'plain',
-      'correct_nested_plain'),
-     ('nested1.yaml',
-      'nested2.yml',
-      'plain',
-      'correct_nested_plain')]
-)
-def test_generating_nested_plain_diff(nested1,
-                                      nested2,
-                                      plain_format,
-                                      correct_nested_plain):
-    diff = generate_diff(
-        form_path_to_test_file(nested1),
-        form_path_to_test_file(nested2),
-        plain_format
-    )
-
-    with open(form_path_to_test_file(correct_nested_plain), 'r') as file:
-        correct_structure = file.read()
-
-        assert diff == correct_structure
-
-
-@pytest.mark.parametrize(
-    'nested1,'
-    'nested2,'
-    'raw_json_format,'
-    'correct_nested_raw_json',
-    [('nested1.json',
-      'nested2.json',
-      'json',
-      'correct_nested_raw_json'),
-     ('nested1.json',
-      'nested2.yml',
-      'json',
-      'correct_nested_raw_json'),
      ('nested1.yaml',
       'nested2.yml',
       'json',
       'correct_nested_raw_json')]
 )
-def test_generating_nested_json_diff(nested1,
-                                     nested2,
-                                     raw_json_format,
-                                     correct_nested_raw_json):
+def test_generating_correct_diff(nested1,
+                                 nested2,
+                                 actual_format,
+                                 expected_format):
     diff = generate_diff(
         form_path_to_test_file(nested1),
         form_path_to_test_file(nested2),
-        raw_json_format
+        actual_format
     )
 
-    with open(form_path_to_test_file(correct_nested_raw_json), 'r') as file:
+    with open(form_path_to_test_file(expected_format), 'r') as file:
         correct_structure = file.read()
 
         assert diff == correct_structure
